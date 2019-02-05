8a27e80d179f315b07f724440e0155c7bf8b7f3e
Ryan Dillinger <rd.nss.services@gmail.com>
2019-02-05 14:31:49 -0600

    posting entries to API successfully!!

--- a2/src/scripts/journal.js
+++ a2/src/scripts/journal.js
@@ -1,6 +1,7 @@
 
+const entryButton = document.querySelector("#entryButton")
 
-fetch("http://localhost:3000/entries")
+const get = fetch("http://localhost:3000/entries")
     .then(entries => entries.json())
     .then(parsedEntries => {
         parsedEntries.forEach(entry => {
@@ -8,6 +9,36 @@ fetch("http://localhost:3000/entries")
             renderJournalEntries(entryAsHTML)
         })
     })
+
+
+
+entryButton.addEventListener(
+    "click",
+    () => {
+        const conceptIN = document.querySelector("#conceptIN").value
+        const moodIN = document.querySelector("#moodIN").value
+        const dateIN = document.querySelector("#dateIN").value
+        const entryIN = document.querySelector("#entryIN").value
+console.log(entryIN)
+console.log(conceptIN)
+        let newJournalEntry = {
+            date: `${dateIN}`,
+            concepts: `${conceptIN}`,
+            entry: `${entryIN}`,
+            mood: `${moodIN}`,
+        }
+
+        fetch("http://localhost:3000/entries", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newJournalEntry)
+        })
+        .then(get)
+    })
+
+
 
 const makeJournalEntryComponent = (allEntries) => {
     return `
@@ -18,6 +49,20 @@ const makeJournalEntryComponent = (allEntries) => {
     </div>`
 }
 
-function renderJournalEntries(x){
+function renderJournalEntries(x) {
     const containerEl = document.querySelector("#entryLog")
-    containerEl.innerHTML += x };
+    containerEl.innerHTML += x
+};
+
+
+// function validateForm(inputValue) {
+//     var x = document.forms["form1"][`${inputValue}`].value;
+//     var regex = /^[a-zA-Z0-9@]+$/;
+//     if (x == "") {
+//       alert("all form entries must be filled out");
+//       return false;
+//     }
+//     if (regex.test(x !== true)){
+//         alert("no special characters or numbers may be used")
+//     }
+//   } 
